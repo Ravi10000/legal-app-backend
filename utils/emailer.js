9db16c77b1e9c5bb06f0da6be5d6bd396b330ac8
ravisince2k <pabhinav400@gmail.com>
2023-06-15 17:33:59 +0530

    add emailer

---
+++ utils/emailer.js
@@ -0,0 +1,43 @@
+import { createTransport } from "nodemailer";
+import hbs from "nodemailer-express-handlebars";
+import path from "path";
+
+async function sendEmail({ to, link }) {
+  const transporter = createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  const handlebarOptions = {
+    viewEngine: {
+      extName: ".hbs",
+      partialsDir: path.resolve("./"),
+      defaultLayout: false,
+    },
+    viewPath: path.resolve("./views"),
+    extName: ".hbs",
+  };
+  transporter.use("compile", hbs(handlebarOptions));
+
+  const mailOptions = {
+    from: `verify email <plagiarism-checker@gmail.com>`,
+    to,
+    subject: "Verify your email",
+    template: "verify-email",
+    context: {
+      apiUrl: process.env.API_URL,
+      link,
+    },
+  };
+
+  return await transporter.sendMail(mailOptions);
+  // transporter.sendMail(mailOptions, (error, info) => {
+  //   error && console.log({ error });
+  //   info && console.log({ info });
+  // });
+}
+
+export default sendEmail;
