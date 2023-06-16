9e04a64fe16ce341cf6f65f378ea517a46e8aa26
ravisince2k <pabhinav400@gmail.com>
2023-06-16 15:24:45 +0530

    notification crud

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -2,17 +2,17 @@ import Notification from "../models/notofication.model.js";
 
 export async function addNotification(req, res) {
   const { message, user, order, service, type } = req.body;
-  if (
-    !message ||
-    !user ||
-    !order ||
-    !service ||
-    !(type === "PRIVATE" || type === "PUBLIC")
-  ) {
+  if (!message || !user || !order || !service || !type) {
     return res.status(400).json({
       status: "error",
       message:
         "required fields: message, user, order, service, type one or more fields are missing",
+    });
+  }
+  if (type !== "PRIVATE" && type !== "PUBLIC") {
+    return res.status(400).json({
+      status: "error",
+      message: "type must be PRIVATE or PUBLIC",
     });
   }
   try {
@@ -31,8 +31,12 @@ export async function addNotification(req, res) {
 }
 
 export async function getAllNotifications(req, res) {
+  const q = {};
+  if (!req.user.isAdmin) {
+    q.user = req.user._id;
+  }
   try {
-    const notifications = await Notification.find();
+    const notifications = await Notification.find(q);
     return res.status(200).json({ status: "success", notifications });
   } catch (err) {
     console.log(err);
@@ -40,11 +44,64 @@ export async function getAllNotifications(req, res) {
   }
 }
 
-export async function updateNotification(req, res) {
-  const { notificationId, is_read, message } = req.body;
+export async function updateNotificationStatus(req, res) {
+  const { notificationId, isRead } = req.body;
+  if (!notificationId || !isRead) {
+    return res.status(400).json({
+      status: "error",
+      message: "required fields: notificationId, isRead",
+    });
+  }
+  if (typeof isRead !== "boolean") {
+    return res.status(400).json({
+      status: "error",
+      message: "isRead must be boolean",
+    });
+  }
   try {
+    const notification = await Notification.findOneAndUpdate(
+      { _id: notificationId, user: req.user._id },
+      { isRead },
+      { new: true }
+    );
+    if (!notification) {
+      return res.status(404).json({
+        status: "error",
+        message: "invalid notificationId or Unauthorised",
+      });
+    }
+    return res.status(200).json({ status: "success", notification });
   } catch (err) {
     console.log(err);
-    res.status(500).json({ status: "error", message: "Internal Server Error" });
+    res.status(500).json({ status: "error", message: err.message });
+  }
+}
+
+export async function deleteNotification(req, res) {
+  const { notificationId } = req.params;
+  if (!notificationId) {
+    return res
+      .status(400)
+      .json({ status: "error", message: "required field: notificationId" });
+  }
+  const q = { _id: notificationId };
+  if (!req.user.isAdmin) {
+    q.user = req.user._id;
+  }
+  try {
+    const notification = await Notification.findOneAndDelete(q);
+    if (!notification) {
+      return res.status(404).json({
+        status: "error",
+        message: "invalid notificationId or Unauthorised",
+      });
+    }
+    return res.status(200).json({
+      status: "success",
+      message: "Notification Deleted Successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ status: "error", message: err.message });
   }
 }
--- routes/notification.route.js
+++ routes/notification.route.js
@@ -2,12 +2,28 @@ import express from "express";
 import {
   addNotification,
   getAllNotifications,
+  updateNotificationStatus,
+  deleteNotification,
 } from "../controllers/notification.controller.js";
+import {
+  checkIfAdmin,
+  fetchUser,
+  isValidUser,
+} from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/", addNotification);
+router.post("/", fetchUser, isValidUser, addNotification);
+router.put("/", fetchUser, isValidUser, updateNotificationStatus);
 
-router.get("/", getAllNotifications);
+router.get("/", fetchUser, isValidUser, checkIfAdmin, getAllNotifications);
+
+router.delete(
+  "/:notificationId",
+  fetchUser,
+  isValidUser,
+  checkIfAdmin,
+  deleteNotification
+);
 
 export default router;
